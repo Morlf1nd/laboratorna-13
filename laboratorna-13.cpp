8bb9e0b74a0077cd5ca8d41e4806421e766360a8
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-29 12:33:55 +0200

    2,3 завдання

--- laboratorna-13.cpp
+++ laboratorna-13.cpp
@@ -1,39 +1,56 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-const int MAX_SIZE = 100;
+const int MAX_ROWS = 5;
+const int MAX_COLS = 3;
+
+
+void initializeArray(int arr[MAX_ROWS][MAX_COLS]) {
+    srand(time(0));
+
+    for (int i = 0; i < MAX_ROWS; ++i) {
+        for (int j = 0; j < MAX_COLS; ++j) {
+            arr[i][j] = rand() % 2;
+        }
+    }
+}
+
+
+void printArray(const int arr[MAX_ROWS][MAX_COLS]) {
+    for (int i = 0; i < MAX_ROWS; ++i) {
+        for (int j = 0; j < MAX_COLS; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int countAdjacentOnes(const int arr[MAX_ROWS][MAX_COLS], int row, int col) {
+    int count = 0;
+
+    for (int i = row - 1; i <= row + 1; ++i) {
+        for (int j = col - 1; j <= col + 1; ++j) {
+            if (i >= 0 && i < MAX_ROWS && j >= 0 && j < MAX_COLS && !(i == row && j == col) && arr[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 int main() {
     setlocale(0, ".1251");
-    int matrix[MAX_SIZE][MAX_SIZE];
-    int size;
+    int array[MAX_ROWS][MAX_COLS];
 
 
-    std::cout << "Введіть розмір квадратної матриці: ";
-    std::cin >> size;
+    initializeArray(array);
+    std::cout << "Двовимірний масив:\n";
+    printArray(array);
 
-    std::cout << "Введіть елементи матриці: " << std::endl;
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            std::cout << "Елемент [" << i + 1 << "][" << j + 1 << "]: ";
-            std::cin >> matrix[i][j];
-        }
-    }
-
-
-    int product = 1;
-    for (int i = 0; i < size; ++i) {
-        if (matrix[i][i] > 0) {
-            product *= matrix[i][i];
-        }
-    }
-
-
-    if (product != 1) {
-        std::cout << "Добуток додатних елементів на головній діагоналі: " << product << std::endl;
-    }
-    else {
-        std::cout << "На головній діагоналі немає додатних елементів або матриця не є квадратною." << std::endl;
-    }
+    int countOnes = countAdjacentOnes(array, 2, 2);
+    std::cout << "Кількість сусідніх елементів, рівних 1, у елементі (2,2): " << countOnes << std::endl;
 
     return 0;
 }
